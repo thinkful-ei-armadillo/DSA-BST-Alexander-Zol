6e55990ef6036b974eab7cf8911baf08cfac9111
AReach <DerAReich@gmail.com>
2019-04-09 14:59:47 -0500

    added maintest function and discuess time complexity

--- bst-drills.js
+++ bst-drills.js
@@ -66,3 +66,16 @@ function thirdLargestNode(t, parent = null) {
 }
 
 // testThirdLargestNode();
+function maintest(arr1, arr2) {
+  // let data = [5, 4, 6, 3, 1, 0, 2];
+  // [3, 1, 5, 2, 4, 6, 0]
+  const sort = (a,b) => {
+    return a - b;
+  };
+  return arr1.sort(sort).join('') === arr2.sort(sort).join('');
+}
+
+console.log(maintest([3, 5, 4, 6, 1, 0, 2], [3, 1, 5, 2, 4, 6, 0]));
+
+//time complexity is O(n log n). Unsure if there's a better solution. Need to compare the inorder traversals of the 'trees' to compare them, to do
+//that we have to sort the arrays. Sort has a time complexity of (n log n) we're doing two sorts and two joins (which are o(n))
